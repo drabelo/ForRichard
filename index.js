b7adef154cc0e9d0b9baed0fbabac685d7d49d65
Dailton Rabelo <dailton.rabelo@ibm.com>
2018-06-29 14:41:50 -0400

    Updating server

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const fetch = require("fetch");
 const app = express();
 
+app.all("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/url", (req, res) => {
