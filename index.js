ab950113b21be5998624b8bd213d7f5dab8f49e5
Dailton Rabelo <dailton.rabelo@ibm.com>
2018-06-29 14:33:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const fetch = require("fetch");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/url", (req, res) => {
+  const options = {
+    headers: {
+      Authorization: "Token 8691b66c9198e00b32eafafaf64f5be5f6e05840"
+    }
+  };
+
+  fetch.fetchUrl(req.query.url, options, function(error, meta, body) {
+    res.send(body.toString());
+  });
+});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
